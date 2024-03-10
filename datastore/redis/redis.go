37bef9787b6a8234b07ca813020059f19583faf0
techquila42 <55506290+techquila42@users.noreply.github.com>
2024-03-10 18:48:29 +0530

    changed redis.go design

--- datastore/redis/redis.go
+++ datastore/redis/redis.go
@@ -1,5 +1,6 @@
 package redis
 
+//maybe implement redis as a struct
 
 import (
     "context"
@@ -7,85 +8,98 @@ import (
 )
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/url"
 	"sync"
+	"strings"
 )
 
+
 type Server struct {
-	host *url.URL 
-	activeConnections int 
-	isAlive bool
-	mutex sync.Mutex
-}
-func (s *Server) setHost(host string){
-	URLhost, err := url.Parse(host)
-	s.host = URLhost
-	if err!=nil{
-		fmt.Println("Error parsing url",err)
-		return
-	}
-
-}
-
-func (s *Server) getHost() *url.URL{
-	return s.host
-}
-
-func (s *Server) getIsAlive() bool {
-	return s.isAlive
-}
-
-func (s *Server) setAliveState(flag bool){
-	s.isAlive=flag 
-}
-
-func (s *Server) getActiveConnections() int {
-	return s.activeConnections;
-}
-
-func (s *Server) incrementConnections() {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
-	s.activeConnections++
-}
-
-func (s *Server) decrementConnections() {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
-	s.activeConnections--
+	Host string
+	ActiveConnections int 
+	IsAlive bool
+	Mutex sync.Mutex
 }
 
 
-var	Servers map[*Server]bool 
-var client 	*redis.Client //read only
 
 
-func Connect(redis_host *url.URL, get_dump bool, dump_location ...string) bool{
-	redis_host.Scheme = ""
-	Servers = make(map[*Server]bool)
-	client = redis.NewClient(&redis.Options{
-        Addr:     redis_host.String(), // Redis server address
+func (s *Server) SetHost(host string){
+	s.Host = strings.Trim(host," ")
+
+}
+
+
+
+func (s *Server) GetHost() string{
+	return s.Host
+}
+
+func (s *Server) GetIsAlive() bool {
+	return s.IsAlive
+}
+
+func (s *Server) SetAliveState(flag bool){
+	s.IsAlive=flag 
+}
+
+func (s *Server) GetActiveConnections() int {
+	return s.ActiveConnections;
+}
+
+func (s *Server) IncrementConnections() {
+	s.Mutex.Lock()
+	defer s.Mutex.Unlock()
+	s.ActiveConnections++
+}
+
+func (s *Server) DecrementConnections() {
+	s.Mutex.Lock()
+	defer s.Mutex.Unlock()
+	s.ActiveConnections--
+}
+
+/*var	Servers map[*Server]bool 
+var mutex sync.Mutex  //mutex for concurrent writes to Servers
+var client 	*redis.Client //read only*/
+
+type Redis struct {
+	Ctx context.Context
+	client 	*redis.Client
+	Mu sync.RWMutex  //mutex for server list
+	Servers map[string] *Server
+	
+}
+
+
+// dump_config=[dump_interval,dump_directory,dump_File]
+func (r *Redis) Connect(redis_host string, get_dump bool, dump_config ...string) bool{
+	redis_host=strings.Trim(redis_host," ")
+	r.Servers = make(map[string]*Server)
+	r.client = redis.NewClient(&redis.Options{
+        Addr:     redis_host, // Redis server address
         Password: "",               // No password
         DB:       0,                // Default DB
     })
-	ctx:= context.Background()
-	_, err := client.Ping(ctx).Result()
+	r.Ctx = context.Background()
+	_, err := r.client.Ping(r.Ctx).Result()
     if err != nil {
+		fmt.Println(err)
         return false
     } else {
 
-		if get_dump  {
+		r.client.ConfigSet(r.Ctx, "save", dump_config[0]+" 1") // set snapshot interval and set to save after change
+
+		if get_dump  {					// configure redis to get the dump file
 		// Set the directory of the dump file
-		_, err := client.ConfigSet(ctx, "dir", dump_location[0]).Result()
+		_, err := r.client.ConfigSet(r.Ctx, "dir", dump_config[1]).Result()
 		if err != nil {
 			fmt.Println("Error setting directory:", err)
 			return false
 		}
 
 		// Set the filename of the dump file
-		_, err = client.ConfigSet(ctx, "dbfilename", dump_location[1]).Result()
+		_, err = r.client.ConfigSet(r.Ctx, "dbfilename", dump_config[2]).Result()
 		if err != nil {
 			fmt.Println("Error setting dbfilename:", err)
 			return false
@@ -96,20 +110,90 @@ func Connect(redis_host *url.URL, get_dump bool, dump_location ...string) bool{
 
 }
 
-func InitServerList(init_servers *[]string){
-	serverSet := GetSetfromDump()
+func (r *Redis) InitServerList(init_servers *[]string) (*map[string]*Server){
+	load_status := r.SyncWithRedisSet()
+	if load_status && len(r.Servers) !=0{
+		return &r.Servers
+	}
+
+	for _, serv_ip := range (*init_servers){
+		server_instance := Server{}
+		server_instance.SetHost(serv_ip)
+		r.Servers[server_instance.GetHost()]=&server_instance
+		//add to redis 
+		_, err := r.client.SAdd(r.Ctx, "Servers", server_instance.GetHost()).Result()
+		if err != nil {
+			fmt.Print("Error while adding member",err)
+			panic(err)
+		}
+	}
+
+	return &r.Servers
 
 }
 
-func GetSetfromDump() {
-	serverSet, err := client.SMembers(context.Background(), "Servers").Result()
+//will reset all object
+func (r *Redis) SyncWithRedisSet() bool{
+	
+	redisSet, err := r.client.SMembers(r.Ctx, "Servers").Result() //returns array of string
+	
 	if err != nil {
-		fmt.Println("Error:", err)
+		fmt.Println("Error in getting servers from dump", err)
+		return false
+	}
+	fmt.Println("sync: ",redisSet)
+	r.Mu.RLock()
+	defer r.Mu.RUnlock()
+	for _,host_name := range redisSet{
+		server_instance := Server{}
+		server_instance.SetHost(host_name)
+		r.Servers[server_instance.GetHost()]=&server_instance
+		
+	}
+	
+	return true
+}
+
+func (r *Redis) AddMember(server_host string){
+	server_instance := Server{}
+	server_instance.SetHost(strings.Trim(server_host, " "))
+	_, present := r.Servers[server_instance.GetHost()]
+	if present {
 		return
 	}
+	r.Mu.Lock() //write lock
+	defer r.Mu.Unlock()
+	r.Servers[server_instance.GetHost()]=&server_instance
+	// modify redis set
+	_, err := r.client.SAdd(r.Ctx, "Servers", server_instance.GetHost()).Result()
+	if err != nil {
+		fmt.Println("Error while adding member",err)
+		panic(err)
+	}
+	
+
 }
 
+func (r *Redis) RemoveMember(server string){
+	r.Mu.Lock()
+	defer r.Mu.Unlock()
+	server = strings.Trim(server, " ")
+	_, present := r.Servers[server]
+	if !present {
+		return
+	}
+	result := r.client.SRem(r.Ctx, "Servers", server)
+    if err := result.Err(); err != nil {
+        fmt.Print("Error while deleting member")
+    }
+	delete(r.Servers,server)
+	
 
+}
 
+func (r *Redis) GetServers() (*map[string]*Server){ 
 
+	return &r.Servers
+	
+}
 
