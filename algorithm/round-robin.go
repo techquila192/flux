1ad114ebed18eb25cb4503f99ddd48f73a0ad939
techquila42 <55506290+techquila42@users.noreply.github.com>
2024-03-13 22:53:04 +0530

    added round robin support

---
+++ algorithm/round-robin.go
@@ -0,0 +1,43 @@
+package algorithm
+
+import (
+	"flux/datastore/redis"
+	"sync"
+)
+
+
+
+func Round_robin(servers *map[string]*redis.Server, servers_mu *sync.RWMutex, vis *map[string]bool, vis_mu *sync.Mutex) (*redis.Server) {
+	 servers_mu.RLock()
+	 defer servers_mu.RUnlock()
+	 vis_mu.Lock()
+	 defer vis_mu.Unlock()
+	 vis_map := *vis
+	 var available int = 0
+	 for name, server := range *servers {
+		if !server.GetIsAlive(){
+			continue
+		}
+		_, check := vis_map[name]
+		if !check {
+			available++
+			break
+		}
+	}
+	if available == 0{
+		*vis = make(map[string]bool)
+		vis_map = *vis
+	} 
+	 for name, server := range *servers {
+		if !server.GetIsAlive(){
+			continue
+		}
+		_, check := vis_map[name]
+		if !check {
+			vis_map[name] = true
+			return server
+		}
+	 }
+	 return nil
+
+}
